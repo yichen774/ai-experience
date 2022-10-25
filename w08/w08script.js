64f16838e8e08e5e4e1d18b18343458803fffbe7
yichen774 <yichen774@gmail.com>
2022-10-25 16:13:57 +0800

    ai-ex w08

--- w08/w08script.js
+++ w08/w08script.js
@@ -14,22 +14,60 @@ class Calculator
     }
     delete()
     {
-
+        this.currentOperand=this.currentOperand.toString.slice(0,-1)
     }
     appendNumber(number)
     {
         if(number ==='.' && this.currentOperand.includes("."))return
         this.currentOperand = this.currentOperand.toString() + number.toString()
     }
-    chooseOperation()
-    {
+    chooseOperation(operation)
+    {   
+        if(this.currentOperand === '')return
+        if(this.previousOperand !== '')
+        {
+            this.compute()
+        }
+        this.operation=operation
+        this.previousOperand=this.currentOperand
+        this.currentOperand=''
     }
     compute()
     {
+        let computation
+        const prey = parseFloat(this.previousOperand)
+        const current = parsefloat(this.currentOperand)
+        if(isNaN(prey)||isNaN(current)) return
+        switch(this.operation)
+        {
+            case "+":
+                computation=prey+current
+                break
+            case "-":
+                computation=prey-current
+                break
+            case "*":
+                computation=prey*current
+                break
+            case "÷":
+                computation=prey/current
+                break
+            default:
+                return
+        }
+        this.currentOperand=computation
+        this.operation=undefined
+        this.previousOperand=''
     }
     updateDisplay()
     {
         this.currentOperandTextElement.innerText = this.currentOperand
+        if(this.operation !=null)
+        {
+            this.previousOperandTextElement.innerText=`${this.previousOperand} ${this.operation}`
+        }
+        else previousOperandTextElement.innerText=''
+        
     }
 }
 
@@ -38,7 +76,7 @@ class Calculator
 const numberButtons=document.querySelectorAll('[data-number]')
 const operationButton=document.querySelectorAll('[data-number]')
 const equalsButton=document.querySelector('[data-equals]')
-const deleteDutton=document.querySelector('[data-delete]')
+const deleteButton=document.querySelector('[data-delete]')
 const allClearButton=document.querySelector('[data-all-clear]')
 const previousOperandTextElement=document.querySelector('[data-previous-operand]')
 const currentOperandTextElement=document.querySelector('[data-current-operand]')
@@ -50,4 +88,25 @@ numberButtons.forEach(button =>{
         calculator.appendNumber(button.innerText)
         calculator.updateDisplay()
     })
+})
+
+operationButton.forEach(button =>{
+    button.addEventListener('click',()=>{
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+equalsButton.addEventListener('click',button=>{
+        calculator.compute()
+        calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click',button=>{
+    calculator.clear()
+    calculator.updateDisplay()
+})
+deleteButton.addEventListener('click',button=>{
+    calculator.compute()
+    calculator.updateDisplay()
 })
